3883498f0ed5407588c8839a5c452a6783c8a41e
David Blezinger <david.w.blezinger@gmail.com>
2023-08-18 16:11:47 +0200

    modified 2 - Watch List

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,3 @@ movie1 = Movie.create(title: 'Wonder Woman 1984', overview: 'Wonder Woman comes 
 movie2 = Movie.create(title: 'The Shawshank Redemption', overview: 'Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison', poster_url: 'https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg', rating: 8.7)
 movie3 = Movie.create(title: 'Titanic', overview: '101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.', poster_url: 'https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg', rating: 7.9)
 movie4 = Movie.create(title: 'Oceans Eight', overview: 'Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.', poster_url: 'https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg', rating: 7.0)
-
-action = List.create(name: 'Action')
-drama = List.create(name: 'Drame')
-
-Bookmark.create(comment: 'Great Movies-Action', movie: movie1, list: action)
-Bookmark.create(comment: 'Great Movies-Action', movie: movie4, list: action)
-Bookmark.create(comment: 'Great Movies-Drama', movie: movie3, list: drama)
-Bookmark.create(comment: 'Great Movies-Drama', movie: movie2, list: drama)
