822917243f564340b5deb9b742c9e59eb35e59ab
David Blezinger <david.w.blezinger@gmail.com>
2023-08-18 16:03:35 +0200

    modified 2 - Watch List

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,7 +20,13 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
